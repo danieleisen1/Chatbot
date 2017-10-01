98432420974f84347d8ec4a5129b4b56f2a99701
Alice <Alice@192.168.1.126>
2017-09-30 21:37:21 -0400

    just got eclipse!

---
+++ ChatbotDAD/src/chatbot/AliceBot.java
@@ -0,0 +1,22 @@
+package chatbot;
+
+public class AliceBot implements Topic {
+
+	public AliceBot() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void talk(String response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isTriggered(String response) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+			
+}
