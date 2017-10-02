184f75c7be111d56eda35821c5289535975f7a2c
Alice <Alice@192.168.1.126>
2017-10-01 23:18:56 -0400

    started proj

--- ChatbotDAD/src/chatbot/ChatbotAliceK.java
+++ ChatbotDAD/src/chatbot/ChatbotAliceK.java
@@ -6,9 +6,11 @@ public class ChatbotAliceK implements Topic {
 	private String goodbyeKeyword;
 	private String secretKeyword;
 	private String response;
+	//private String[] responses;
 
 	public ChatbotAliceK() {
-		keywords = new String[] { "go yale" , "go bulldogs" , "yale sucks" , "harvard sucks" , "harvard is bad" ,"" ,  "go crimson" , "crimson" , "don't have greek life" , "no greek life" , "greek life" , "which house is the best" , "" , "stuff","things","whatever", "nothing"};
+		keywords = new String[] { "go yale" , "go bulldogs" , "yale sucks" , "harvard sucks" , "harvard is bad" ,"harvard sucks at football" , "harvard is bad at football" , "harvard vs yale football game" , "football game" , "yale vs harvard football game" , "harvard yale football game" , "yale harvard football game" , "harvard will win" , "harvard always win" , "yale will win" , "yale always wins" , "john harvard" , "annenberg" , "annenberg is great" , "food" , "charles river" , "cambridge is beautiful" , "boston is beautiful" , "boston" , "cambridge" ,  "go crimson" , "crimson" , "don't have greek life" , "no greek life" , "greek life" , "which house is the best" , "party" , "parties" ,  "stuff","things","whatever", "nothing" , " " };
+		//responses = new String[] {"Did you mean Harvard? Yeah go Harvard! " , "What's good with you? You mispelled Crimson." , "Right on!!!" , "You've been brainwashed to epic proportions " , " " ,  };
 		goodbyeKeyword= "bye";
 		secretKeyword= "pug";
 		response = "";
@@ -43,7 +45,22 @@ public class ChatbotAliceK implements Topic {
 			else if( response.contains( keyword )) {
 				switch( keyword ) {
 				case "stuff": ChatbotMain.print(" Alice: Yeah, some stuff man");  break;
-				case "whatever": ChatbotMain.print("Alice: Talk more specifically, not just whatever");  break;
+				case "whatever": ChatbotMain.print("Alice: Talk more specifically, not just whatever"); 
+				case "go yale" : ChatbotMain.print(" Did you mean Harvard? Yeah go Harvard!"); 
+				case "go bulldogs" : ChatbotMain.print("What's good with you? You mispelled Crimson."); 
+				case "yale sucks" : ChatbotMain.print("Right on!!!"); 
+				case "harvard sucks" : ChatbotMain.print("You've been brainwashed to epic proportions");
+				case "harvard is bad" : ChatbotMain.print("If bad means good then totally! Harvard is bad.");
+				case "harvard sucks at football" : ChatbotMain.print("At least our students get into graduate school");
+				case "harvard is bad at footbal" : ChatbotMain.print("Not like you're any better");
+				case "john harvard" : ChatbotMain.print("John Harvard did not actually found Harvard! There are three main myths about John Harvard's statue in the Harvard Yard. 1) It isn't actually John Harvard /n 2) John Harvard isn't the founder of Harvard, even though 'founder' is engraved on the statue /n 3) Harvard was founded in 1636, not 1638, as the engraving on the statue claims.");
+				case "annenberg is great" : ChatbotMain.multiLinePrint("Not like you're any better");
+				case "go crimson" : ChatbotMain.multiLinePrint("Yeah!!!! GO CRIMSON");
+				case "which house is the best": ChatbotMain.multiLinePrint("Yeah!!!! GO CRIMSON");
+				case "party" : ChatbotMain.multiLinePrint("Party?!!! Yeah don't worry we have those. ");
+				case "parties" : ChatbotMain.multiLinePrint("Party?!!! Yeah don't worry we have those. ");
+				
+				break;
 				}
 				
 			}
@@ -76,5 +93,7 @@ public class ChatbotAliceK implements Topic {
 		
 		
 	}
+	
+	
 
 }
